d97af828565787855007d3142e1c8beb4f319b15
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-10-22 11:29:41 +0700

    tes tensorflow model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app.core import config, model_predict
+from app.core import config, model_predict, model_keras
 from fastapi import FastAPI, File, UploadFile
 import shutil
 import os
@@ -27,3 +27,24 @@ async def predict_image(file: UploadFile = File(...)):
     os.remove(file_location)
     
     return {"predict": predict}
+
+@app.post("/predict/keras/")
+async def predict_image(file: UploadFile = File(...)):
+    # create folder if not exist
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+    # check if the file is an image
+    if file.content_type.split('/')[0] != 'image':
+        return {"predict": "Invalid file type"}
+    # Save the uploaded file to the tmp directory
+    file_location = f"tmp/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    # Perform prediction
+    predict = model_keras.predict(file_location)
+    
+    # Delete the file after prediction
+    os.remove(file_location)
+    
+    return predict
