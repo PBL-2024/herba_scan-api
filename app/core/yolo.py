93d4ab246085f43fe6add0e979ef35847698b5b7
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-11-27 13:06:25 +0700

    Refactor image segmentation to improve mask handling and adjust image resizing

--- app/core/yolo.py
+++ app/core/yolo.py
@@ -9,26 +9,34 @@ from app.core.config import Config
 def segment_image(image_path, model, labels=None, device='cpu'):
     # Baca gambar
     img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     h, w = img.shape[:2]
 
     # Prediksi menggunakan model YOLO
-    result = model.predict(
-        image_path,
-        # points=[[w / 1.5, h / 1.5], [w / 0.5, h / 0.5]],
-        # labels=labels or [1],
-        conf=0.40,
-        iou=0.70,
-        device=device,
-        imgsz=400,
-        # save=True
-    )
-
+    try:
+        result = model.predict(
+            image_path,
+            points=[w / 2, h / 2],
+            labels=labels or [1],
+            conf=0.60,
+            iou=0.70,
+            device=device,
+            imgsz=416,
+            # save=True
+        )
+    except Exception as e:
+        return img
+    
+    if result is None:
+        return img
     # Akses masks
     masks = result[0].masks  # results adalah objek YOLO
     if masks is not None:
+        if masks.data is None:
+            return img
         mask_array = masks.data.cpu().numpy()  # Konversi ke NumPy array
     else:
-        raise ValueError("No masks detected.")
+        return img
 
     # Ambil gambar asli
     orig_img = result[0].orig_img
@@ -49,23 +57,16 @@ def segment_image(image_path, model, labels=None, device='cpu'):
                 largest_mask = binary_mask
 
     if largest_mask is None:
-        raise ValueError("No valid contours found.")
+        return img
 
-    orig_img = cv2.resize(orig_img, (400 ,400))
-    largest_mask = cv2.resize(largest_mask, (400,400))
-    # Terapkan mask terbesar ke gambar asli
-    masked_image = cv2.bitwise_and(orig_img, orig_img, mask=largest_mask)
-
+    orig_img = cv2.resize(orig_img, (416 ,416))
+    largest_mask = cv2.resize(largest_mask, (416,416))
+    
     # Hitung bounding box dari mask terbesar
     x, y, w, h = cv2.boundingRect(largest_mask)
 
     # Potong area berdasarkan bounding box
-    cropped_image = masked_image[y:y + h, x:x + w]
-    cropped_mask = largest_mask[y:y + h, x:x + w]
-    cropped_image = cropped_image.copy()
-
-    # Ubah latar belakang hitam menjadi putih
-    cropped_image[cropped_mask == 0] = [255, 255, 255]
+    cropped_image = orig_img[y:y + h, x:x + w]
 
     # Konversi ke RGB untuk ditampilkan dengan matplotlib
     cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
@@ -78,7 +79,7 @@ def predict_image(img_path,model):
     url = "https://predict.ultralytics.com"
     headers = {"x-api-key": Config.API_KEY}
     data = {"model": Config.MODEL_URL, "imgsz": 640, "conf": 0.85, "iou": 0.70}
-    cropped_image = segment_image(img_path, model, labels=[1,1], device='cpu')
+    cropped_image = segment_image(img_path, model, labels=[1], device='cpu')
     file_path = "tmp/cropped_image.jpg"
     cv2.imwrite(file_path, cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR))
     with open(file_path, "rb") as f:
